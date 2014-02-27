28740dde86d3b77fd1ca66324ca4047502fd8bdc
garbagemanonfire <nilesvm@gmail.com>
2014-02-26 23:41:38 -0800

    Setup Rails App, Backbone, and Marionette, up to first view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require lib/underscore
+//= require lib/backbone
+//= require lib/marionette
+//= require_tree ./backbone/config
+//= require backbone/app
+//= require_tree ./backbone/apps
